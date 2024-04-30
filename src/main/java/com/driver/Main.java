77b5732d343de09fa86ae5db622dcd31abe8de9d
Saikumar Ketireddi <kethireddysaikumar7@gmail.com>
2024-04-30 15:17:45 +0530

    error check

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,12 +1,12 @@
 package com.driver;
 
-class A {
+public class A {
     String meth() {
         return "Invoking method from class A";
     }
 }
 
-class B extends A {
+public class B extends A {
     @Override
     String meth() {
         return "Method is overridden in Extended class B";
