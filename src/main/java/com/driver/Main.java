bbe67a2c14bafc12b05890e120d90a7e0d1364df
Saikumar Ketireddi <kethireddysaikumar7@gmail.com>
2024-04-30 15:14:10 +0530

    error checking

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,26 +1,28 @@
 package com.driver;
 
-class A{
-    public String meth(){
+class A {
+    String meth() {
         return "Invoking method from class A";
     }
 }
-class B extends A{
-    public String meth(){
-        return "Method is overridden in Extendend class B";
+
+class B extends A {
+    @Override
+    String meth() {
+        return "Method is overridden in Extended class B";
     }
 }
+
 public class Main {
-    public static void main(String[]args){
-        A obj = new A();
-        //calling a method
-        String resTask3 = obj.meth();
-        System.out.println("task 3 result: "+ resTask3);
-        //polymorphism
-        A obj1 = new B();
-        //callin a method in B class
-        String resTask5 = obj1.meth();
-        System.out.println("task 5 result: "+ resTask5);
+    public static void main(String[] args) {
+        // Task 3
+        B objB = new B();
+        String resultTask3 = objB.meth(); // Calling method meth of class A with object of class B
+        System.out.println("Task 3 Result: " + resultTask3);
+
+        // Task 5
+        String resultTask5 = objB.meth(); // Calling overridden method from object of class B
+        System.out.println("Task 5 Result: " + resultTask5);
     }
 }
 
