278460106e4a4c0d87c9ff92d6c050a7a19967aa
Saikumar Ketireddi <kethireddysaikumar7@gmail.com>
2024-05-02 15:36:25 +0530

    code check

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,12 +1,12 @@
 package com.driver;
 
-public class A {
+class A {
     String meth() {
         return "Invoking method from class A";
     }
 }
 
-public class B extends A {
+class B extends A {
     @Override
     String meth() {
         return "Method is overridden in Extended class B";
@@ -17,12 +17,7 @@ public class Main {
     public static void main(String[] args) {
         // Task 3
         B objB = new B();
-        String resultTask3 = objB.meth(); // Calling method meth of class A with object of class B
-        System.out.println("Task 3 Result: " + resultTask3);
-
-        // Task 5
-        String resultTask5 = objB.meth(); // Calling overridden method from object of class B
-        System.out.println("Task 5 Result: " + resultTask5);
+        System.out.println(objB.meth());
     }
 }
 
